27a509218a43fec71eb18e390a12a444a10da0f4
Y4shwant <yashu042006@gmail.com>
2025-07-24 00:30:17 +0530

    onsite 2 final

--- onsites/daemon.sh
+++ onsites/daemon.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-set -x
-log_file="/var/log/daemon.log"
-specified_threshold=200
-
+log_file="daemon.log"
+specified_threshold=5
 start_daemon(){
-    daemon &
+    daemon &>/dev/null &
     PID=$!
     echo "$PID" > /tmp/daemondelete.log 
     echo "daemon started successfully"   
@@ -17,37 +15,43 @@ stop_daemon(){
 
 
 daemon(){
-    declare -A networks
-    declare -A runtime
-    while true; do
-        inp=`docker ps --format "{{.ID}} {{.Networks}}"`
-        while IFS= read -a containers; do
-            for i in ${container[@]}; do
-                IFS="+" read -a containers_split <<< "$i"
-                networks[${containers_split[1]}]+="$containers_split[0] "
-                time=`docker inspect --format='{{.State.StartedAt}}' ${containers_split[0]}`
-                time=`echo "$(date +%s) -$(date +%s -d "$time")" | bc`
-                runtime[${containers_split[0]}]=$time
 
-            done
-            for i in ${networks[@]}; do
-                IFS=" " read -a IDs <<< "${networks[$i]}"
-                count=$(wc -w <<< "${networks[$i]}")
-                counter=0
-                for j in ${IDs[@]}; do
-                    if [ "${runtime[$j]}" -gt "$specified_threshold" ]; then
-                        ((counter++)) 
-                    fi
-                done
-                if [ "$counter" -eq "$count"]; then
-                    for j in ${IDs[@]}; do
-                        docker stop $j 2>/dev/null
-                        echo "container: '$(docker inspect --format "{{.Name}}")' stopped" >> $log_file
-                    done
+    
+    while true; do
+        declare -A runtime
+        declare -A networks
+        mapfile -t containers <<< "$(docker ps --format '{{.ID}}+{{.Networks}}')"
+        for i in ${containers[@]}; do
+            echo "$i"
+            IFS="+" read -a containers_split <<< "$i"
+            networks[${containers_split[1]}]="${networks[${containers_split[1]}]} ${containers_split[0]}"
+            time=`docker inspect --format='{{.State.StartedAt}}' ${containers_split[0]}`
+            time=`echo "$(date +%s) -$(date +%s -d "$time")" | bc`
+            runtime[${containers_split[0]}]=$time
+
+        done
+        for i in ${!networks[@]}; do
+            if [ "$i" == "bridge" ];then
+                continue
+            fi
+            IFS=" " read -a IDs <<< "${networks[$i]}"
+            echo "${IDs[@]}"
+            count=$(wc -w <<< "${networks[$i]}")
+            counter=0
+            for j in ${IDs[@]}; do
+                if [ "${runtime[$j]}" -gt "$specified_threshold" ]; then
+                    ((counter++)) 
                 fi
-            done    
-        done <<< "$inp"
+            done
+            if [ "$counter" -eq "$count" ]; then
+                for j in ${IDs[@]}; do
+                    docker stop $j 2>/dev/null
+                    echo "container: '$(docker inspect --format "{{.Name}}" $j)' stopped at $(date)" >> $log_file
+                done
+            fi
+        done    
         sleep 5
+        
     done
 }
 
@@ -58,7 +62,7 @@ case $1 in
     "stop")
         stop_daemon
         ;;
-    "*")
+    *)
         echo "usage: daemon [start/stop]"
         ;;
 esac
